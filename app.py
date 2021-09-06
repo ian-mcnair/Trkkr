84e1fcb24d96b7adf7999f00def52e89823be381
Ian <imcnair13@gmail.com>
2021-09-06 14:50:18 -0500

    can I commit?

---
+++ app.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import streamlit as st
+import views
+
+views.title()
+
---
+++ views.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import pandas as pd
+
+def title():
+    st.title("Testing the Local App")
