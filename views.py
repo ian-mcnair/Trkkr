a5ef56e403dc273c9775b6e520ba61d25cb39f6a
Ian <imcnair13@gmail.com>
2021-09-07 21:29:44 -0500

    Changed form to be more flexible

--- views.py
+++ views.py
@@ -7,29 +7,44 @@ def welcome():
     st.write(f'## Welcome back user!')
 
 def form():
-    with st.form(key='my_form'):
-        uid = st.selectbox(
-            'Select your user',
-            ('Ian', 'Gladys', 'Linda', 'Wayne', 'Rando')
+    amt = 0
+    reps = 0
+    
+    uid = st.selectbox(
+        'Select your user',
+        ('Ian', 'Gladys', 'Linda', 'Wayne', 'Rando')
+    )
+    measure = st.radio(
+        'Measurement', [
+            'Weight',
+            'Bench Press', 
+            'Deadlift',
+            'Running'
+        ])
+    if measure == 'Running':
+        amt_label = 'Distance Ran'
+        rep_label = 'Time'
+    else:
+        amt_label = 'Amount (lbs):'
+        rep_label = 'Reps (if applicable)'
+
+    amt = st.number_input(
+        label=amt_label,
+        min_value = 0,
+        step = 1
         )
-        measure = st.radio(
-            'Measurement', [
-                'Weight',
-                'Bench Press', 
-                'Deadlift'
-            ])
-        amt = st.number_input(
-            label='Amount (lbs):',
-            min_value = 0,
-            step = 1
-            )
-        reps = st.number_input(
-            label='Reps (if applicable):',
-            min_value = 0,
-            step = 1
-            )
-        #ingestion = st.radio('Ingestion', ['Real-time', 'Historical'])
-        submission = st.form_submit_button(label='Submit')
+    reps = st.number_input(
+        label=rep_label,
+        min_value = 0,
+        step = 1
+        )
+    ingestion = st.radio('Ingestion', [
+        'Set', 'Goal'
+    ])
+    
+    submit_button = st.button('Submit Data')
+    st.write(submit_button)
+    if submit_button:
         utils.submit_info({
             'uid':uid, 
             'measurement': measure, 
