a00da3c43cb843e619e19ffee5fd2c2f98f1d85c
Wayne Chim <wayne.chim01@gmail.com>
2022-04-19 20:36:37 -0400

    20220419 gsheets connect and day over day

--- utils.py
+++ utils.py
@@ -4,119 +4,51 @@ it will probably go into utils
 '''
 
 import pandas as pd
-import numpy as np
-import json
+from datetime import datetime
+
 import plotly.express as px
 import plotly.io as pio
 import streamlit as st
-from gsheetsdb import connect
+import time
 
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread_dataframe as gd
+
+# Setting up Google Sheets Connect
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+google_key_file = 'trkkr-343522-8d5890e74f07.json'
+credentials = ServiceAccountCredentials.from_json_keyfile_name(google_key_file, scope)
+gc = gspread.authorize(credentials)
+
+spreadsheet_key = '1nAIsnqIm4qpaaFSqdog2zFfO39zfXip4LyvF9AbpYs8'
+worksheet_name = 'main'
+
+workbook = gc.open_by_key(spreadsheet_key)
+sheet = workbook.worksheet(worksheet_name)
+values = sheet.get_all_values()
+
+main = pd.DataFrame(values[1:], columns=values[0])
+
+# Converting Timestamp from String to Datetime
+#main['timestamp'] = [datetime.strptime(timestamp, 'MM/dd/yyyy HH:mm:ss') for timestamp in main['timestamp']]
+main['timestamp'] = pd.to_datetime(main['timestamp'], infer_datetime_format=True)
+main['wt_lb'] = main['wt_lb'].astype(float)
+main['wt_kg'] = main['wt_kg'].astype(float)
+
+# Loading Data
 pio.templates
+@st.cache(ttl=600, suppress_st_warning=True)
 
-conn = connect()
-sheet_url = st.secrets['public_gsheets_url']
+def load_table():
+    return main
 
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    return rows
+# Submit Form
+def submit(new_entry={}):
+    update = main.append(new_entry, ignore_index=True)
+    gd.set_with_dataframe(sheet, update)
 
-def build_table():
-    rows = run_query(f'SELECT * FROM "{sheet_url}"')
-    df = pd.DataFrame(columns=['timestamp', 'user', 'wt_lb', 'wt_kg'])
-    for row in rows:
-        _ = pd.DataFrame([[row.timestamp, row.user, row.wt_lb, row.wt_kg]],
-                         columns=['timestamp', 'user', 'wt_lb', 'wt_kg'])
-        df = df.append(_, ignore_index=True)
-    return df
-
-
-# Data Submission
-#@st.cache(suppress_st_warning=True)
-def submit_info(info = []):
-    df = pd.read_csv('dummy.csv')
-
-
-    #fname = users_ls[str(info['uid'])]
-
-    #if info['attribute'] == 'Body Weight':
-    #    st.write(f'''{fname} weighs {info['amt']} pounds!''')
-
-    #elif info['attribute'] == 'Running':
-    #    st.write(f'''{fname} ran {info['reps']} miles in {info['amt']} minutes!''')
-    
-    #elif info['attribute'] == 'Lifts':
-    #    st.write(f'''{fname} lifted {info['amt']} pounds {info['reps']} times for {info['lift']}!''')
-    
-    df = df.append(info, ignore_index=True)
-    #df.to_csv('dummy.csv', index=False)
-    st.table(df)
-
-# Calculating ORM for Lifts
-# def normalize(amt, reps):
-#     if reps == 1:
-#         orm = amt
-#     else:
-#         orm = round(amt*(1+(0.0333*reps)), 2)
-
-#     return orm
-
-# Convert to actionable weights to use in the gym
-# def gym_convert(wt, pct=1):
-#     #adjusted_orm = 0.9*orm
-
-#     cur_total_wt = pct*wt
-
-#     side_wt = (cur_total_wt - 45)/2
-
-#     if side_wt % 5.0 > side_wt % 10.0:
-#         side_wt = side_wt - side_wt % 10.0
-#     else:
-#         side_wt = side_wt - side_wt % 5.0
-#     side_wt = round(side_wt, 0)
-
-#     gym_total_wt = side_wt*2 + 45
-
-#     set_up = {'Gym Wt': gym_total_wt,
-#               'Side Wt': side_wt}
-
-#     return set_up
-
-# Populate table with work out set suggestions
-# def weight_table(orm):
-#     st.write(f'''User has a one-rep max of {orm} pounds!''')
-
-#     tbl = pd.read_csv('wt_table.csv')
-
-#     pct_ls = [] 
-
-#     for i in tbl['% of ORM']:
-#         pct = float(i.strip('%'))/100
-#         pct_ls.append(pct)
-
-#     tbl['Actual Wt (lbs)'] = np.array(pct_ls) * orm
-#     tbl['Gym Wt (lbs)'] = [gym_convert(wt)['Gym Wt'] for wt in tbl['Actual Wt (lbs)']]
-#     tbl['Wt to Put On (lbs)'] = [gym_convert(wt)['Side Wt'] for wt in tbl['Actual Wt (lbs)']]
-
-#     st.table(tbl)
-
-# Summarize user performance over time
-# def summary_graph(info=[]):
-#     df = pd.read_csv('dummy.csv')
-#     df = df.append(info, ignore_index=True)
-#     set_df = df[(df['lift'] == info['lift']) & (df['ingestion'] == 'Set')].reset_index()
-#     goal = int(df[(df['lift'] == info['lift']) & (df['ingestion'] == 'Goal')]['orm'])
-#     fig = px.line(set_df,
-#                   x=(set_df.index+1).astype(str),
-#                   y='orm',
-#                   title='User Performance Summary',
-#                   labels={'x': 'Days',
-#                           'orm': ''})
-#     fig.add_hline(y=goal, line_color='lightgrey', line_dash='dash')
-#     fig.add_hrect(y0=goal-5, y1=goal+5, line_width=0, fillcolor='yellow', opacity=0.2)
-
-#     st.plotly_chart(fig)
-
-# Export data to Google Sheet
-def export_info(info=[]):
-    pass
+    with st.spinner('One moment...'):
+        time.sleep(1)
+        st.success("All done!")
--- views.py
+++ views.py
@@ -6,124 +6,66 @@ set up the layout of the application. Any othe rfunctionality can probably resid
 import pandas as pd
 import numpy as np
 from datetime import datetime
+
 import streamlit as st
-#import streamlit_authenticator as stauth
+
 import utils
-import time
 import plots
 
-
 def welcome():
+    st.set_page_config(page_title='Trkkr',
+                       page_icon=':muscle:',
+                       layout='wide')
     st.write(f"# Trkkr")
 
 def form():
-    amt = np.empty(1)
-    reps = np.empty(1)
-    lift = ''
-    
-    df = utils.build_table()
+    weight = np.empty(1)
+
+    df = utils.load_table()
 
     # User
     t = st.empty()
     t.write(f'### Welcome back, User!')
 
-    
     name = st.selectbox(
         'Who is this?',
         ('Select User', 'Ian McNair', 'Wayne Chim')
     )
 
-    chart = st.empty()
+    dod = st.empty()
 
     if name == 'Select User':
-        # Wayne, please delete if you think it is unneeded -----------------------------
-        # user_ls = df.user.value_counts().index.tolist()
-        # user_df = pd.DataFrame()
-        # for u_name in user_ls:
-        #     u_wt = df[df.user==u_name].wt_lb.tolist()
-        #     user_df[u_name] = u_wt
         st.plotly_chart(plots.general_weight(df))       
-        
-
     else:
+        plots.current_weight(df, name)
         fname = name.split()[0]
         t.write(f'### Welcome back, {fname}!')
-        # Wayne, please delete the below if you think it is unneeded --------------------
-        # u_df = df[df.user == name]
-        # u_df = u_df.reset_index()
-        # chart.line_chart(u_df['wt_lb'])
-        fig, slope = plots.weight(df, name)
+        fig, slope = plots.weight_track(df, name)
         st.plotly_chart(fig)
         st.write(f'{name} is changing weight by {round(slope, 2)} per day on average')
-
     # WE SHOULD DEF PUSH THIS (below) INTO ANOTHER FUNCTIONS MAYBE???
     # LIKE A FORMATTING.PY FILE???? 
     # Just seems like a lot happening and its hard to read/follow at a glance.
     
-    # Attribute
-    
-    attribute = st.radio(
-        'Attribute', ['Body Weight']
-    )
+    # Input fields
 
-    if attribute == 'Body Weight':
-        amt_label = 'Weight'
-        lift = 'Body Weight'
-        #reps = 1
+    weight = st.number_input(label = 'Weight',
+                             min_value = 0.0,
+                             step = 0.1)
 
-        amt = st.number_input(
-            label = amt_label,
-            min_value = 0.0,
-            step = 0.1)
+    # Unit of Measure
+    uom = st.radio('Unit of Measure',
+                   ['Pounds', 'Kilograms'])   
 
-    # metric
-    metric = st.radio(
-        'Metric', ['Pounds', 'Kilograms']
-    )
-
-    if metric == 'Pounds':
-        lb = round(amt,2)
-        kg = round(amt/2.2,2)
-        st.write(f'{lb} lb or {kg} kg')
-    elif metric == 'Kilograms':
-        lb = round(amt*2.2,2)
-        kg = round(amt,2)
-        st.write(f'{lb} lb or {kg} kg')
-
-    '''elif attribute == 'Lifts':
-        amt_label = 'Weight (lbs)'
-        rep_label = 'Reps'
-
-        amt = st.number_input(
-            label = amt_label,
-            min_value = 0,
-            step = 1)
-
-        reps = st.number_input(
-            label = rep_label,
-            min_value = 0,
-            step = 1)
-
-        lift = st.selectbox(
-            'Lift Type', ('Deadlift', 'Bench Press', 'Squat', 'Military Press'))
-    elif attribute == 'Running':
-        amt_label = 'Time'
-        rep_label = 'Distance Ran (in miles)'
-        lift = 'Running'
-
-        amt = st.text_input(
-            label = amt_label,
-            )
-
-        reps = st.number_input(
-            label = rep_label,
-            min_value = 0,
-            step = 1)'''
-
-    # Data Ingestion    
-    '''ingestion = st.radio(
-        'Ingestion', ['Set', 'Goal']
-        )'''
+    if weight > 0:
+        if uom == 'Pounds':
+            lb = round(weight, 2)
+            kg = round(weight/2.2, 2)
+            st.write(f'{lb} lb or {kg} kg')
+        elif uom == 'Kilograms':
+            lb = round(weight*2.2,2)
+            kg = round(weight,2)
+            st.write(f'{lb} lb or {kg} kg')
     
     # Data Submission
     submit_button = st.button('Submit Data')
@@ -138,43 +80,9 @@ def form():
             'wt_lb': lb,
             'wt_kg': kg
         }
-        
-        with st.spinner('One moment...'):
-            time.sleep(1)
-            st.success("All done!")
 
         st.json(entry)
-        #utils.submit_info(entry)
-
-    '''if submit_button:
-        
-        dt = str(datetime.now())
-        
-        if attribute == 'Lifts':
-            orm = utils.normalize(amt, reps)
-        else:
-            orm = amt
-        
-        info = {
-            'uid': uid,
-            'datetime': dt,
-            'attribute': attribute,
-            'lift': lift,
-            'amt': str(amt),
-            'reps': reps,
-            'orm': str(orm),
-            'ingestion': ingestion
-            }
-
-        utils.submit_info(info)
-
-        if info['attribute'] == 'Lifts' and info['ingestion'] == 'Set':
-            utils.summary_graph(info)
-            utils.weight_table(orm)
-        else:
-            pass
-
-        #utils.print_log()'''
+        utils.submit(entry)
 
 def goals():
     pass
