66b1d014c2b245df2e68d47eb1241db934d25c91
Wayne Chim <wayne.chim01@gmail.com>
2022-04-21 22:12:25 -0400

    20220421 added new metrics- moving avg, change since beginning, leaderboard position, first draft of leaderboard, slight form modification

--- format.py
+++ format.py
@@ -11,7 +11,28 @@ def for_datatable(main):
     main['timestamp'] = pd.to_datetime(main['timestamp'], infer_datetime_format=True)
     main['wt_lb'] = main['wt_lb'].astype(float)
     main['wt_kg'] = main['wt_kg'].astype(float)
+    main['date'] = [_.date() for _ in main.timestamp]
     return main
+
+def for_usertable(data, user):
+    df = data[data.user == user].copy()
+    return df
+
+def for_leaderboard(data):
+    _ = data.groupby('user')
+    df = pd.concat([_.head(1), _.tail(1)]).drop_duplicates().sort_values('user').reset_index(drop=True).dropna()
+    df['lb_pct'] = df.groupby('user')['wt_lb'].pct_change() * 100
+    df['lb_pct'] = df['lb_pct'].round(2).astype('string') + '%'
+    df['days_since'] = df.groupby('user')['date'].diff()
+    df['days_since'] = df['days_since'].astype('string')
+    leaderboard = df.groupby('user').tail(1).dropna(axis=0)[['user', 'lb_pct', 'days_since']].sort_values('lb_pct').reset_index(drop=True)
+    leaderboard.rename(columns={'user': 'User',
+                                'lb_pct': '% Change',
+                                'days_since': 'Days Since Start'},
+                       inplace=True)
+    leaderboard.index = leaderboard.index + 1
+    #print(leaderboard)
+    return leaderboard
 
 def for_weight(uom, input):
     # uom - Unit of measure
@@ -21,5 +42,5 @@ def for_weight(uom, input):
     elif uom == 'Kilograms':
         lb = round(input*2.2,2)
         kg = round(input,2)
-    print(f'{lb} pounds or {kg} kilograms')
+    #print(f'{lb} pounds or {kg} kilograms')
     return lb, kg
--- plots.py
+++ plots.py
@@ -9,6 +9,8 @@ import streamlit as st
 from datetime import datetime
 from datetime import timedelta
 
+import format
+
 def weight_track(data, user): # <- I want to add timeline as well
     '''
     Creates a plot based on the user (and eventually a time period)
@@ -55,8 +57,7 @@ def day_over_day(data, user):
     '''
     Display day over day weight change for user (currently only in lbs)
     '''
-    df = data[data.user == user].copy()
-    df['date'] = [_.date() for _ in df.timestamp]
+    df = format.for_usertable(data, user)
     today = df.date.max()
     lb_today = df[df.date == today]['wt_lb'].values[0]
 
@@ -64,17 +65,57 @@ def day_over_day(data, user):
         prev_day = df[df.date.argsort() == len(df) - 2].date.values[0]
         lb_prev = df[df.date == prev_day]['wt_lb'].values[0]
         lb_diff = round(lb_today - lb_prev, 2)
-        label = 'Day Over Day Weight Change (lbs)'
         delta_color = 'inverse'
     else:
-        label = 'First Weigh In (lbs)'
         lb_diff = 0.0
         delta_color = 'normal'
+    label = 'Day Over Day Weight Change (lbs)'
     return label, lb_today, lb_diff, delta_color
 
 def last_updated(data, user):
-    df = data[data.user == user].copy()
-    df['date'] = [_.date() for _ in df.timestamp]
+    df = format.for_usertable(data, user)
     today = df.date.max()
     st.write('Last updated ', str(today))
+
+def moving_avg(data, user):
+    '''
+    Display 5-day moving average change for user (currently only in lbs)
+    '''
+    df = format.for_usertable(data, user)
+    today = df.date.max()
+    lb_today = df[df.date == today]['wt_lb'].values[0]
+
+    if len(df) >= 5:
+        lb_5dma = df.sort_values(by='date')[-5:]['wt_lb'].mean()
+        lb_5dma = round(lb_5dma, 2)
+        lb_diff = round(lb_today - lb_5dma, 2)
+        delta_color = 'inverse'
+    else:
+        lb_diff = 0.0
+        delta_color = 'normal'
+    label = '5-Day Moving Average (lbs)'
+    return label, lb_5dma, lb_diff, delta_color
+
+def first_weighin(data, user):
+    '''
+    Display first weigh in for user and compare to most recent weigh in (currently only in lbs)
+    '''
+    df = format.for_usertable(data, user)
+    today = df.date.max()
+    lb_today = df[df.date == today]['wt_lb'].values[0]
+
+    lb_first = df.sort_values(by='date', ignore_index=True)[:1]['wt_lb'].values[0]
+    lb_diff = round(lb_today - lb_first, 2)
+    delta_color = 'inverse'
+    label = 'First Weigh-In (lbs)'
+    return label, lb_first, lb_diff, delta_color
+
+def leaderboard_pos(data, user):
+    '''
+    Display user current position on leaderboard
+    '''
+    pos = data[data.User == user].index.values[0]
+    label = 'Leaderboard Position'
+    return label, pos
+    
 
