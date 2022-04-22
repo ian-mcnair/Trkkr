7aefd8605c60a79fe67132dd1e7fd490c91f7930
Wayne Chim <wayne.chim01@gmail.com>
2022-04-22 00:15:03 -0400

    quickfix leaderboard ranking, remove random data, adjust ian real data, metric displays

--- format.py
+++ format.py
@@ -20,12 +20,17 @@ def for_usertable(data, user):
 
 def for_leaderboard(data):
     _ = data.groupby('user')
-    df = pd.concat([_.head(1), _.tail(1)]).drop_duplicates().sort_values('user').reset_index(drop=True).dropna()
+    df = pd.concat([_.head(1), _.tail(1)]).sort_values('user').reset_index(drop=True).dropna()
     df['lb_pct'] = df.groupby('user')['wt_lb'].pct_change() * 100
     df['lb_pct'] = df['lb_pct'].round(2).astype('string') + '%'
     df['days_since'] = df.groupby('user')['date'].diff()
     df['days_since'] = df['days_since'].astype('string')
-    leaderboard = df.groupby('user').tail(1).dropna(axis=0)[['user', 'lb_pct', 'days_since']].sort_values('lb_pct').reset_index(drop=True)
+
+    leaderboard = df.groupby('user').tail(1).dropna(axis=0)[['user', 'lb_pct', 'days_since']].sort_values('lb_pct')
+    leaderboard['elg'] = ['False' if int(_.split()[0]) == 0 else 'True' for _ in leaderboard['days_since']]
+    leaderboard = leaderboard.sort_values('elg', ascending=False).reset_index(drop=True)
+    leaderboard = leaderboard.drop(['elg'], axis=1)
+    
     leaderboard.rename(columns={'user': 'User',
                                 'lb_pct': '% Change',
                                 'days_since': 'Days Since Start'},
